21ae5fddce33a482158b5c96c3d6dcef6373adca
Amitav Roy <reachme@amitavroy.com>
2023-10-08 10:46:38 +0530

    Video 4 - Small changes to create process of Resource

--- app/Filament/Resources/CustomerResource/Pages/CreateCustomer.php
+++ app/Filament/Resources/CustomerResource/Pages/CreateCustomer.php
@@ -3,17 +3,32 @@
 namespace App\Filament\Resources\CustomerResource\Pages;
 
 use App\Filament\Resources\CustomerResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
-use Illuminate\Database\Eloquent\Model;
 
 class CreateCustomer extends CreateRecord
 {
     protected static string $resource = CustomerResource::class;
 
-    protected function handleRecordCreation(array $data): Model
+    protected function mutateFormDataBeforeCreate(array $data): array
     {
         $data['user_id'] = auth()->user()->id;
 
-        return parent::handleRecordCreation($data);
+        return $data;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return CustomerResource::getUrl('index');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title('A new customer was added. Horray!!!')
+            ->success()
+            ->body('A new customer got added. Let us try and add more')
+            ->duration(3000)
+            ->send();
     }
 }
