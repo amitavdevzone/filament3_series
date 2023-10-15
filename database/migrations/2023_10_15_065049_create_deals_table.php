4e5c808d519e1364006d0a346594ad97131ec47d
Amitav Roy <reachme@amitavroy.com>
2023-10-15 12:32:09 +0530

    Migrations and Model changes

---
+++ database/migrations/2023_10_15_065049_create_deals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->enum('status', ['Contacted', 'Arranged call']); // need to add more
+            $table->foreignIdFor(Customer::class)->index();
+            $table->foreignIdFor(User::class, 'owner_id')->index();
+            $table->float('value', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deals');
+    }
+};
