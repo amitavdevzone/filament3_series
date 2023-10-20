69e0a75a4931e2c2eee30590d3be31939c70fec3
Saurabh Singh <saurabhsingh.7636@gmail.com>
2023-10-20 22:19:59 +0530

    add owner at the time of create with trait in resource and header action in table of relation manager

---
+++ app/Actions/OwnedCreateAction.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Actions;
+
+use Filament\Tables\Actions\CreateAction;
+
+class OwnedCreateAction
+{
+    public static function make()
+    {
+        return CreateAction::make()
+            ->mutateFormDataUsing(function (array $data): array {
+                $data['owner_id'] = auth()->id();
+                return $data;
+            });
+    }
+
+}
