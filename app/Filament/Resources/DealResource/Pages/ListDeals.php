ea7e37e57096d94eb0efed4f187c2c0cbef2bffb
Amitav Roy <reachme@amitavroy.com>
2023-11-22 07:05:07 +0530

    Deals screen now has tabs

--- app/Filament/Resources/DealResource/Pages/ListDeals.php
+++ app/Filament/Resources/DealResource/Pages/ListDeals.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\DealResource\Pages;
 
 use App\Filament\Resources\DealResource;
+use App\Models\Deal;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
 use Illuminate\Database\Eloquent\Builder;
 
 class ListDeals extends ListRecords
@@ -14,6 +16,24 @@ class ListDeals extends ListRecords
     protected function getTableQuery(): Builder
     {
         return parent::getTableQuery()->orderByDesc('id');
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All Deals')
+                ->icon('heroicon-m-user-group')
+                ->badge(Deal::count()),
+            'me' => Tab::make('My Deals')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('owner_id', auth()->user()->id))
+                ->icon('heroicon-m-user')
+                ->badge(Deal::where('owner_id', auth()->user()->id)->count()),
+        ];
+    }
+
+    public function getDefaultActiveTab(): string|int|null
+    {
+        return 'me';
     }
 
     protected function getHeaderActions(): array
